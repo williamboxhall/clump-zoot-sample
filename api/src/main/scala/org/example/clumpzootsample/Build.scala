280b8bce7474c89d2a578eea64d4246c79f283e4
William Boxhall <william@soundcloud.com>
2015-02-23 18:20:14 +0100

    Utility method to hide client/server blahblah

---
+++ api/src/main/scala/org/example/clumpzootsample/Build.scala
@@ -0,0 +1,35 @@
+package org.example.clumpzootsample
+
+import java.net.InetSocketAddress
+
+import com.twitter.finagle.builder.{ClientBuilder, ServerBuilder}
+import com.twitter.finagle.http.{Http, Request, RichHttp}
+import net.fwbrasil.zoot.core.mapper.JacksonStringMapper
+import net.fwbrasil.zoot.core.{Api, Client, Server}
+import net.fwbrasil.zoot.finagle.{FinagleClient, FinagleServer}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object Build {
+
+  private implicit val mirror = scala.reflect.runtime.currentMirror
+  private implicit val mapper = new JacksonStringMapper
+
+  def serverFor[T <: Api: Manifest](name: String, port: Int, controller: T) = {
+    val serverBuilder = ServerBuilder()
+      .codec(RichHttp[Request](Http()))
+      .bindTo(new InetSocketAddress(port))
+      .name(name)
+    val server = new FinagleServer(Server[T](controller), serverBuilder.build)
+    println(s"Started $name service.")
+    server
+  }
+
+  def clientFor[T <: Api: Manifest](port: Int) = {
+    val clientBuilder = ClientBuilder()
+      .codec(RichHttp[Request](Http()))
+      .hosts(s"localhost:$port")
+      .hostConnectionLimit(5)
+    Client[T](FinagleClient(clientBuilder.build))
+  }
+}
