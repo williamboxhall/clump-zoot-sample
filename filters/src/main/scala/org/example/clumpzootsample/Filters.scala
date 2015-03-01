1ba45f3720c965a8e6e0f83616ab1791fe5f068a
William Boxhall <william@soundcloud.com>
2015-03-01 17:47:22 +0100

    Can't get exceptions handled in the filter chain

--- filters/src/main/scala/org/example/clumpzootsample/Filters.scala
+++ filters/src/main/scala/org/example/clumpzootsample/Filters.scala
@@ -3,12 +3,25 @@ package org.example.clumpzootsample
 import net.fwbrasil.zoot.core.filter.Filter
 import net.fwbrasil.zoot.core.request.Request
 
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object Filters {
   val requestLogFilter =
     new Filter {
       override def apply(request: Request, next: Service) = {
         println(s"request $request")
         next(request)
+      }
+    }
+
+  val exceptionLogFilter =
+    new Filter {
+      override def apply(request: Request, next: Service) = {
+        val response = next(request)
+        response.onFailure {
+          case _ => println(s"Exception was thrown!")
+        }
+        response
       }
     }
 }
